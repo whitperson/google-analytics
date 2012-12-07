5cf06bba3178e9accb2e29a41ac8b1ac02b18e49
whitperson <whitperson@gmail.com>
2012-12-07 14:27:31 -0500

    first commit

---
+++ app/models/ninja.rb
@@ -0,0 +1,3 @@
+class Ninja < ActiveRecord::Base
+  attr_accessible :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleAnalytics::Application.config.secret_token = 'f9ceb0aeb79fb33bf8b16b6d1f0fcd5a94c2691e313cbf7413fb5f6edbe73a0246790de540304a8ba00c705627f6b81306ae0748c6fb6c9b8f0b41f24d46ae97'
